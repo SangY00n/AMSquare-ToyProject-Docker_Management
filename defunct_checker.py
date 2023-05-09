c961e43bbd4657eb56e43b71a8b0d418ab08e7ae
SangY00n <dbs1367@postech.ac.kr>
2023-05-09 14:33:35 +0900

    Update: defunct_checker.py to print multiple container IDs with most defuncts.

--- defunct_checker.py
+++ defunct_checker.py
@@ -2,23 +2,28 @@ import os
 import time
 import schedule
 import requests
+from datetime import datetime
+from collections import OrderedDict
+
+TIMEOUT = 1 # Schedule every TIMEOUT minutes
+THRESHOLD = 20 # Find the container IDs with most defuncts when the total number of defuncts > THRESHOLD
+TOPCONTAINER = 3 # 찾아서 알림으로 보낼 Container ID 최대 개수, defuncts 개수가 많은 순으로 최대 TOPCONTAINER 개만큼 찾는다.
 
 
-TIMEOUT = 30 # schedule every TIMEOUT minutes
-THRESHOLD = 20
 
 def shell_run(command):
     # print('[SHELL] {command}'.format(command=command))
     output = os.popen(command).read()
     return output
 
-def get_docker_container_id_list():
-    container_ids = shell_run("docker ps -a | awk \'{print $1}\'")
-    container_id_list = container_ids.split('\n')
-    container_id_list = list(filter(None, container_id_list)) # filtering to remove empty string element
-    return container_id_list
+def get_docker_container_id_name_list():
+    container_id_name_list = shell_run("docker ps --format \"table {{.ID}}\t{{.Names}}\"").split('\n')
+    container_id_name_list = list(filter(None, container_id_name_list)) # filtering to remove empty string element
+    container_id_name_list = [(line.split()[0][0:6], line.split()[1]) for line in container_id_name_list if line.split()[0] != 'CONTAINER']
+    
+    return container_id_name_list
 
-# pid를 받아 해당 process가 돌아가고 있는 container의 id를 찾아서 반환
+# pid를 받아 해당 process가 돌아가고 있는 container의 id를 포함하는 process 정보 라인을 찾아서 반환
 # 못 찾을 경우 빈 문자열 '' 반환
 def get_container_line(pid):
     next_ppid = pid
@@ -47,51 +52,66 @@ def get_container_line(pid):
     
     return container_line
 
-def find_container_id_with_most_defunct():
-    total_defunct_num = int(shell_run("ps -ef | grep defunct | grep -v grep | grep -v 'defunct_checker' | wc -l"))
-
+def get_defunct_ppid_count():
     defunct_ppid_list = shell_run("ps -ef | grep defunct | grep -v grep | grep -v 'defunct_checker' | awk '{print $3}'").split('\n')
     defunct_ppid_list = list(filter(None, defunct_ppid_list)) # filtering to remove empty string element
-    print()
     defunct_ppid_count = {}
     for ppid in defunct_ppid_list:
         if defunct_ppid_count.get(ppid): defunct_ppid_count[ppid]+=1
         else: defunct_ppid_count[ppid]=1
-
-    print('ppids with defunct: ', defunct_ppid_count)
-    max_defunct_ppid = max(defunct_ppid_count, key=defunct_ppid_count.get)
-    print('ppid with the most defunct: ', max_defunct_ppid)
-
-    ## Find container id
-    container_line = get_container_line(max_defunct_ppid)
-    if container_line == '':
-        return '', max(defunct_ppid_count.values()), total_defunct_num
-
-    print('the process creating container: ', container_line)
-
-    ## Check if there is such an id among the docker container ids
-    container_id=''
-    for i in get_docker_container_id_list():
-        if i in container_line:
-            container_id = i
-            break
-    if container_id=='':
-        print('There is no such id among the docker container ids')
-    print(container_id)
-    return container_id, max(defunct_ppid_count.values()), total_defunct_num
+    defunct_ppid_count = OrderedDict(sorted(defunct_ppid_count.items(), key=lambda item: -item[1]))
+    return defunct_ppid_count
 
 def check_defunct():
-    total_defunct_num = int(shell_run('ps -ef | grep defunct | grep -v grep | wc -l'))
+    total_defunct_num = int(shell_run("ps -ef | grep defunct | grep -v grep | grep -v 'defunct_checker' | wc -l"))
+
+    now = datetime.now()
+    print("###########################", now, "###########################")
+    print("The result of [ps -ef | grep defunct | grep -v grep | grep -v 'defunct_checker']:")
+    print(shell_run("ps -ef | grep defunct | grep -v grep | grep -v 'defunct_checker'"))
+
     if total_defunct_num > THRESHOLD:
-        container_id, defunct_num, total_defunct_num = find_container_id_with_most_defunct()
-        print(f"The container {container_id} has {defunct_num} defuncts among {total_defunct_num} defuncts.")
+        container_id_ppid_defunct_list = []
+        defunct_ppid_count = get_defunct_ppid_count()
+        num_appended_container_id=0
+        docker_container_id_name_list = get_docker_container_id_name_list()
+        for ppid, count in defunct_ppid_count.items():
+            container_line = get_container_line(ppid)
+            if container_line=='': continue
+            container_id=''
+            container_name=''
+            for i in docker_container_id_name_list: 
+                # TODO: 아이디에 더해서 container 이름도 추가하기
+                if i[0] in container_line:
+                    container_id = i[0]
+                    container_name = i[1]
+                    break
+            container_id_ppid_defunct_list.append((container_name, container_id, ppid, count))
+            num_appended_container_id += 1
+            if num_appended_container_id >= TOPCONTAINER:
+                break
+
+        
+        printFormat = '%-10s\t%-8s\t%-8s\t%-8s'
+        
+        print(printFormat % ("[ConName]", "[ConID]", "[PPID]", "[Dfncts]"))
+        for i in container_id_ppid_defunct_list:
+            print(printFormat % (i[0][:8], i[1], i[2], i[3]))
+
         ## Notification is needed.
-        push_content = f"The container {container_id} has {defunct_num} defuncts among {total_defunct_num} defuncts."
+        push_content=''
+        push_content += (printFormat % ("[ConName]", "[ConID]", "[PPID]", "[Dfncts]")) + '\n'
+        for i in container_id_ppid_defunct_list:
+            push_content += (printFormat % (i[0][:8], i[1], i[2], i[3])) + '\n'
         push_notification(push_content)
+        print("\nNofication is sended.")
+        
     else:
         print(f"Number of defunct processes is less than THRESHOLD.\n \
                 The Number of defunct processes: {total_defunct_num}. \
         ")
+    
+    print("\n\n---------------------------------------------------------------------------------\n\n")
 
 
 def push_notification(content):
@@ -102,9 +122,11 @@ def push_notification(content):
         })
 
 def main():
-    print(f"Run the defunct checker every {TIMEOUT} minutes.")
-    # schedule.every(TIMEOUT).minutes.do(check_defunct)
-    schedule.every(10).seconds.do(check_defunct)
+    print(f"Run the defunct checker every {TIMEOUT} minutes.\n\n")
+    check_defunct()
+
+    schedule.every(TIMEOUT).minutes.do(check_defunct)
+    # schedule.every(10).seconds.do(check_defunct)
     while True:
         schedule.run_pending()
         time.sleep(1)
